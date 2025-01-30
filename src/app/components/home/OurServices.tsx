63a525045e19e2d311058c120b5043a4e853303d
mustafa <mustafasa3d2@gmail.com>
2025-01-30 02:14:20 +0200

    add nav bar and first section

--- src/app/components/home/OurServices.tsx
+++ src/app/components/home/OurServices.tsx
@@ -13,7 +13,7 @@ function OurServices({ locale }: { locale: string }) {
       <Container>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
           {[1,2,3,4].map((item, index) => (
-            <div key={index} className="bg-gray-200/70 rounded-3xl text-center p-5">
+            <div key={index} className="bg-gray-200/70 rounded-3xl text-center px-5 py-7">
               <div className="mb-2 flex items-center justify-center">
                 <Icon
                   icon="basil:phone-outline"
@@ -23,7 +23,7 @@ function OurServices({ locale }: { locale: string }) {
               <h3 className="text-lg font-bold text-primary mb-2">
                 طلب رفع مقاسات
               </h3>
-              <p className="text-paragText mb-3 text-sm">
+              <p className="text-paragText mb-5 text-sm">
                 هنا نبذة تفصيلية بسيطة الخدمة المقدمة من الشركة
               </p>
 
