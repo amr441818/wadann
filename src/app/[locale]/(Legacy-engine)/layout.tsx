ee6cb3880628fc43a2bfc635e92b1307f7bbcfe2
mustafa <mustafasa3d2@gmail.com>
2025-01-30 03:39:00 +0200

    add footer

--- src/app/[locale]/(Legacy-engine)/layout.tsx
+++ src/app/[locale]/(Legacy-engine)/layout.tsx
@@ -43,10 +43,8 @@ export default async function Layout({ children, params }: LayoutProps) {
                         <Header lang={locale} />
                        
                         <div className="fixed bottom-4 right-4 flex gap-5 flex-col z-[999]">
-                            <div className="bg-secondary hover:bg-primary p-3 rounded-full duration-300">
-                                <Icon className="text-white text-3xl" icon="basil:phone-outline" />
-                            </div>
-                            <div className="bg-secondary hover:bg-primary p-3 rounded-full duration-300">
+                          
+                            <div className="bg-primary hover:bg-black/80 p-3 rounded-full duration-300">
                                 <Icon className="text-white text-3xl" icon="hugeicons:whatsapp" />
                             </div>
                         </div>
--- src/app/components/shared/footer/index.tsx
+++ src/app/components/shared/footer/index.tsx
@@ -1,90 +1,109 @@
-import Container from "../container"
-import { Icon } from "@iconify/react"
-import Image from "next/image"
-import MainLink from "../main-link"
-import logoServer from "@/public/logoServer.svg"
+import Container from "../container";
+import { Icon } from "@iconify/react";
+import Image from "next/image";
+import MainLink from "../main-link";
+import logoServer from "@/public/logo.png";
 
 // import logo from "../../../../../public/logo.png"
 
-
-
-
-
 const Footer = async (params: any) => {
-    const { locale: language } = await params
-    console.log("language", language)
-    const cols = [
-        {
-            title: "Main Pages",
-            links: ["Home", "About us", "List Your Property"],
-        },
-        {
-            title: "About",
-            links: ["About us", "List Your Property"],
-        },
-        {
-            title: "Contact",
-            links: ["About us", "List Your Property"],
-        },
-    ]
+  const { locale: language } = await params;
+  console.log("language", language);
+  const cols = [
+    {
+      title: "روابط هامة",
+      links: ["الرئيسية", "من نحن", "المنتجات", "صمم بابك"],
+    },
+    {
+      title: "",
+      links: ["خدماتنا", "عملائنا وشركاؤنا", "المدونة", "تواصل معنا"],
+    },
+  ];
 
-    return (
-        <footer className=" bg-black text-white px-5 lg:px-0">
-            <Container>
-                {/* logo and links */}
-                <div className="grid grid-cols-1 md:grid-cols-4 py-14 gap-3">
-                    <div className="flex justify-center md:justify-start max-md:items-center ">
-                        <Image className="w-[200px] h-[54px]" src={logoServer} width={200} height={54} alt="logo" />
-                        {/* <Image src="logo.png" width={100} height={100} alt="logo" /> */}
-                    </div>
+  return (
+    <footer className=" bg-primary text-white px-5 lg:px-0">
+      <Container>
+        {/* logo and links */}
+        <div className="grid grid-cols-1 md:grid-cols-4 py-14 gap-3">
+          <div className="flex justify-center md:justify-start max-md:items-center ">
+            <Image
+              className="w-[145px] h-[145px] rounded-3xl overflow-hidden"
+              src={logoServer}
+              width={200}
+              height={54}
+              alt="logo"
+            />
+            {/* <Image src="logo.png" width={100} height={100} alt="logo" /> */}
+          </div>
 
-                    {cols.map((col) => (
-                        <div key={col.title}>
-                            <h3 className="font-semibold mb-2">{col.title}</h3>
-                            <div className="flex flex-col ">
-                                {col.links.map((link) => (
-                                    <MainLink
-                                        className="my-2 lg:my-5 block text-white/70"
-                                        key={link}
-                                        locale={language}
-                                        href="list-your-property"
-                                    >
-                                        {link}
-                                    </MainLink>
-                                ))}
-                            </div>
-                        </div>
-                    ))}
-                </div>
+          {cols.map((col) => (
+            <div key={col.title}>
+              <h3 className={`font-semibold ${col.title ? "mb-2" : "mb-8"}`}>
+                {col.title}
+              </h3>
+              <div className="flex flex-col">
+                {col.links.map((link) => (
+                  <MainLink
+                    className="my-1 lg:my-3 block text-white/70"
+                    key={link}
+                    locale={language}
+                    href="list-your-property"
+                  >
+                    {link}
+                  </MainLink>
+                ))}
+              </div>
+            </div>
+          ))}
 
-                {/* copyright */}
-                <div className="border-t border-gray-600 flex flex-col-reverse md:flex-row items-center justify-between gap-y-5 pt-5 pb-14">
-                    <div className="flex gap-5">
-                        <MainLink locale={language} href="home">
-                            <Icon icon="fa6-brands:twitter" />
-                        </MainLink>
-                        <MainLink locale={language} href="home">
-                            <Icon icon="fa6-brands:linkedin-in" />
-                        </MainLink>
-                        <MainLink locale={language} href="home">
-                            <Icon icon="fa6-brands:facebook-f" />
-                        </MainLink>
-                    </div>
-                    <div className="flex gap-10">
-                        <MainLink className="!text-white/70" locale={language} href="home">
-                            FAQs
-                        </MainLink>
-                        <MainLink className="!text-white/70" locale={language} href="home">
-                            Privacy Policy
-                        </MainLink>
-                        <MainLink className="!text-white/70" locale={language} href="home">
-                            Terms & Conditions
-                        </MainLink>
-                    </div>
-                </div>
-            </Container>
-        </footer>
-    )
-}
+          <div>
+            <h3 className={`font-semibold mb-10`}>تابعنا من خلال</h3>
+            <div className="flex flex-wrap gap-4">
+              <MainLink
+                className="border border-white rounded-full p-2 w-[33px] h-[33px] flex items-center justify-center"
+                locale={language}
+                href="home"
+              >
+                <Icon icon="basil:whatsapp-outline" />
+              </MainLink>
+              <MainLink
+                className="border border-white rounded-full p-2 w-[33px] h-[33px] flex items-center justify-center"
+                locale={language}
+                href="home"
+              >
+                <Icon icon="ic:outline-snapchat" />
+              </MainLink>
+              <MainLink
+                className="border border-white rounded-full p-2 w-[33px] h-[33px] flex items-center justify-center"
+                locale={language}
+                href="home"
+              >
+                <Icon icon="hugeicons:instagram" />
+              </MainLink>
+              <MainLink
+                className="border border-white rounded-full p-2 w-[33px] h-[33px] flex items-center justify-center"
+                locale={language}
+                href="home"
+              >
+                <Icon icon="pajamas:twitter" />
+              </MainLink>
+              <MainLink
+                className="border border-white rounded-full p-2 w-[33px] h-[33px] flex items-center justify-center"
+                locale={language}
+                href="home"
+              >
+                <Icon icon="ph:tiktok-logo" />
+              </MainLink>
+            </div>
+        {/* copyright */}
+            <div className="mt-7">© جميع الحقوق محفوظة - وادان للأبواب الخشبية</div>
+          </div>
+        </div>
 
-export default Footer
+        
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
