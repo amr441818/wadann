40be726a2dc4ac7619cc608b4b1d7c06d838bcaa
mustafa <mustafasa3d2@gmail.com>
2025-02-02 12:25:07 +0200

    responsive in slider

--- src/app/components/shared/main-header/main-header-slider/MainHeaderSlider.tsx
+++ src/app/components/shared/main-header/main-header-slider/MainHeaderSlider.tsx
@@ -1,84 +1,82 @@
-"use client"
-import { Swiper, SwiperSlide } from "swiper/react"
+"use client";
 
+import "swiper/css";
+import "./MainHeaderSlider.css";
 
-import "swiper/css"
-import "./MainHeaderSlider.css"
+import { Swiper, SwiperSlide } from "swiper/react";
 
-
-import { Autoplay } from "swiper/modules"
-import { useTranslations } from "next-intl"
-
-import Image from "next/image"
-import Container from "../../container"
-import { Files } from "lucide-react"
-
+import { Autoplay } from "swiper/modules";
+import Container from "../../container";
+import { Files } from "lucide-react";
+import Image from "next/image";
+import { useTranslations } from "next-intl";
 
 interface Slider {
-    id: number;
-    sort: number;
-    name: string;
-    content: string;
-    image: string;
-    link: string | null;
-  }
- type sliderProps = {
-    files: Slider[]
- }
-export default function MainHeaderSlider(props:sliderProps) {
+  id: number;
+  sort: number;
+  name: string;
+  content: string;
+  image: string;
+  link: string | null;
+}
+type sliderProps = {
+  files: Slider[];
+};
+export default function MainHeaderSlider(props: sliderProps) {
+  console.log("filse", props.files);
 
-  console.log("filse" , props.files)
+  const t = useTranslations();
 
-
-    const t = useTranslations() 
-  
-   
-
-
-    return (
-        <>
-            <div className="">
-                <Swiper
-                    spaceBetween={0}
-                    centeredSlides={true}
-                    //   autoplay={{
-                    //     delay: 3000,
-                    //     disableOnInteraction: false,
-                    //   }}
-                     speed={700}
-                      loop
-                    
-                    modules={ [Autoplay]}
-                    className="MainHeaderSlider z-0 "
-                    style={{ zIndex: "0px" }}
-                >
-
-
-
-
-                    {props?.files?.map((file, index)=> {
-                        return (<> <SwiperSlide key={index} >
-                            <div className="grid grid-cols-12 h-full w-full bg-primary">
-                                <div className="flex col-span-6 w-full items-end ">  
-                                      <Image width={569} height={419} src={file?.image} alt={file?.name} className="!w-[569px] h-[419px] " />
-                                      
-                                      </div>
-                                <div className="flex w-full col-span-6 items-center justify-end">
-
-                                    <div className="flex flex-col items-start w-full text-white gap-[26px]">
-                                        <h1 className="text-[35px] font-bold w-[334px]">    {file?.name}</h1>
-                                        <p className="w-[291px] text-start text-[16px] text-gray-300">  {file?.content}</p>
-                                    </div>
-                                </div>
-
-                            </div>
-                        
-                        </SwiperSlide></>)
-                    })}
-                 
-                   
-                </Swiper>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="">
+        <Swiper
+          spaceBetween={0}
+          centeredSlides={true}
+          //   autoplay={{
+          //     delay: 3000,
+          //     disableOnInteraction: false,
+          //   }}
+          speed={700}
+          loop
+          modules={[Autoplay]}
+          className="MainHeaderSlider z-0 "
+          style={{ zIndex: "0px" }}
+        >
+          {props?.files?.map((file, index) => {
+            return (
+              <>
+                {" "}
+                <SwiperSlide key={index}>
+                  <div className="grid grid-cols-12 h-full w-full bg-primary">
+                    <div className="flex col-span-12 lg:col-span-6 w-full items-end justify-center ">
+                      <Image
+                        width={569}
+                        height={419}
+                        src={file?.image}
+                        alt={file?.name}
+                        className="!w-[250px] h-[150px] lg:!w-[569px] lg:h-[419px] object-cover"
+                      />
+                    </div>
+                    <div className="flex col-span-12 lg:col-span-6 items-center justify-center lg:justify-end">
+                      <div className="flex flex-col items-start w-full text-white gap-[26px]">
+                        <h1 className="text-[35px] font-bold lg:w-[334px] mx-auto">
+                          {" "}
+                          {file?.name}
+                        </h1>
+                        <p className="lg:w-[291px] text-start text-[16px] text-gray-300 mx-auto">
+                          {" "}
+                          {file?.content}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </SwiperSlide>
+              </>
+            );
+          })}
+        </Swiper>
+      </div>
+    </>
+  );
 }
