90de4ab8991424aa18355b5d0cd1c5400742b626
amr salah <amrro.salah@gmail.com>
2025-03-04 23:58:36 +0200

    rounded none in inputs

--- src/app/components/shared/reusableComponents/CustomTextAria.tsx
+++ src/app/components/shared/reusableComponents/CustomTextAria.tsx
@@ -21,7 +21,7 @@ const CustomTextAria = (props: CustomTextAriaProps) => {
                 placeholder={props?.placeholder}
                 required={props.required ? props.required : false}
                 
-                className="bg-white border border-gray-300 text-gray-900 text-sm rounded-[25px] focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                className="bg-white border border-gray-300 text-gray-900 text-sm  focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
             />
         </div>
     )
--- src/app/components/shared/reusableComponents/InputComponent.tsx
+++ src/app/components/shared/reusableComponents/InputComponent.tsx
@@ -20,7 +20,7 @@ const InputComponent = (props: customInputProps) => {
                 required={props.required ? props.required : false}
                 disabled={props.disabled ? props.disabled : false}
                 
-                className="bg-white p-4 border border-gray-300 text-gray-900 text-sm rounded-[25px]  focus:ring-primary-600 focus:border-primary-600 block w-full  dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                className="bg-white p-4 border border-gray-300 text-gray-900 text-sm   focus:ring-primary-600 focus:border-primary-600 block w-full  dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
             />
         </div>
     )
